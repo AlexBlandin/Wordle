36b69cec8d70b1b978603605515a356d08f29a71
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-07-30 11:35:23 +0100

    YAPF

--- aidel.py
+++ aidel.py
@@ -4,28 +4,32 @@ from functools import partial
 from operator import itemgetter
 from collections import defaultdict
 
-def sorted_dict(d, key=itemgetter(1), reverse=False): return dict(sorted(d.items(), key=key, reverse=reverse))
-data = Path(__file__).parent/"data"
-words = (data/"nordle"/"words.txt").read_text().splitlines()
+def sorted_dict(d, key = itemgetter(1), reverse = False):
+  return dict(sorted(d.items(), key = key, reverse = reverse))
+
+data = Path(__file__).parent / "data"
+words = (data / "nordle" / "words.txt").read_text().splitlines()
 ws = set(words) # working set
 
-def inverse(): return {c: set() for c in "abcdefghijklmnopqrstuvwxyz"}
+def inverse():
+  return {c: set() for c in "abcdefghijklmnopqrstuvwxyz"}
+
 by_letter = inverse()
 by_column = {i: inverse() for i in range(5)}
 by_negate = inverse()
 
 for d in words:
-  for i,c in enumerate(d):
+  for i, c in enumerate(d):
     by_letter[c].add(d)
     by_column[i][c].add(d)
   for s in set("abcdefghijklmnopqrstuvwxyz") - set(d):
     by_negate[s].add(d)
 
 aware = []
-know = ["?"]*5
+know = ["?"] * 5
 
 def now():
-  global aware,know
+  global aware, know
   print("Contains", *aware)
   print("Known positions", *know)
 
@@ -43,38 +47,40 @@ def stat():
   3: {"e": 318, "n": 182, "s": 171, "a": 162, "l": 162, "i": 158, "c": 150, "r": 150, "t": 139, "o": 132, "u": 82, "g": 76, "d": 69, "m": 68, "k": 55, "p": 50, "v": 45, "f": 35, "h": 28, "w": 25, "b": 24, "z": 20, "y": 3, "x": 3, "j": 2},
   4: {"e": 422, "y": 364, "t": 253, "r": 212, "l": 155, "h": 137, "n": 130, "d": 118, "k": 113, "a": 63, "o": 58, "p": 56, "m": 42, "g": 41, "s": 36, "c": 31, "f": 26, "w": 17, "i": 11, "b": 11, "x": 8, "z": 4, "u": 1}}
   """
-  letter, column = defaultdict(int), {i:defaultdict(int) for i in range(5)}
+  letter, column = defaultdict(int), {i: defaultdict(int) for i in range(5)}
   for d in ws:
-    for i,c in enumerate(d):
+    for i, c in enumerate(d):
       column[i][c] += 1
       letter[c] += 1
-  letter = sorted_dict(letter, reverse=True)
-  column = {i:sorted_dict(d, reverse=True) for i,d in column.items()}
+  letter = sorted_dict(letter, reverse = True)
+  column = {i: sorted_dict(d, reverse = True) for i, d in column.items()}
   print(len(ws))
   print(letter)
   for col, letters in column.items():
     print(f"{col}: {letters}")
 
-def guess(ws=ws):
+def guess(ws = ws):
   if len(ws) > 10:
     print(*sample(sorted(ws), 10))
   else:
     print(*ws)
 
-def unique(ws=ws): # guess but all letters different
-  guess(list(filter(lambda p: len(set(p))==5, ws)))
+def unique(ws = ws): # guess but all letters different
+  guess(list(filter(lambda p: len(set(p)) == 5, ws)))
 
 def has(ltr: str, col: int): # you know it's there, but that it's not in this column
   global aware, know
   aware.append(ltr)
   ws.difference_update(by_negate[ltr])
   ws.difference_update(by_column[col][ltr])
+
 def no(*letters: list[str]):
-  global aware,know
+  global aware, know
   for ltr in letters:
     ws.difference_update(by_letter[ltr])
+
 def at(ltr: str, col: int):
-  global aware,know
+  global aware, know
   if ltr in aware: aware.remove(ltr)
   know[col] = ltr
   for d, s in by_column[col].items():
@@ -85,15 +91,14 @@ def code(*pairs):
   two_words = partial(str.split, maxsplit = 1)
   for guess, code in map(two_words, pairs):
     for col, (letter, colour) in enumerate(zip(guess, code)):
-      match colour:
-        case "n":
-          no(letter)
-        case "g":
-          at(letter, col)
-        case "y":
-          has(letter, col)
+      if colour == "g":
+        at(letter, col)
+      elif colour == "y":
+        has(letter, col)
+      else:
+        no(letter)
 
-if __name__=="__main__":
+if __name__ == "__main__":
   while 1:
     pair = input("Guess and colours[gyn]: ")
     code(pair)
